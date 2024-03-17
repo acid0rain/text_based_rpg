c7f9c77f28dafa4b0942a8a9d4fdfe7d7d5f9ecf
acid0rain <hugodcarneiro@gmail.com>
2024-03-17 17:42:20 +0100

    added Troll class and cleaned up level up function to allow choosing skill to levell up

--- text based RPG.py
+++ text based RPG.py
@@ -8,6 +8,9 @@ character_level = 1
 experience = 0
 next_level = 10
 
+#progression counter
+progress = False
+
 # Accessing individual values
 character_stats = [0, 0, 0, 0] #hp, str, magic, dex list
 
@@ -47,7 +50,7 @@ weapon = False
 while not weapon:
     char_weapon = input("Choose your weapon: \nSword \nAxe \nStaff \nBow \n")
 
-    if char_weapon.lower() in ["sword", "axe", "staff", "bow"]:
+    if char_weapon.lower() in ["sword", "axe", "staff", "bow"]: #checking against list of weapons
         print("Nice! You have chosen the " + char_weapon)
         weapon = True
     else:
@@ -56,6 +59,8 @@ while not weapon:
 
 #define level up function
 def level_up (character_stats, experience, next_level): #take 3 arguments, stats, current xp and next level xp
+    global strength, magic, dexterity
+
     if experience >= next_level:
         print("Congratulations you levelled up! Your HP is increased by 5!")
         character_stats[0] += 5  # add 2 to HP
@@ -67,21 +72,24 @@ def level_up (character_stats, experience, next_level): #take 3 arguments, stats
             if increase_skill.lower() == "strength":
                 print("Strength increased. What else?")
                 character_stats[1] += 1  # add 1 to strength
+                strength = character_stats[1]
                 skillpoints -= 1
             elif increase_skill.lower() == "magic":
                 print("Magic increased. What else?")
                 character_stats[2] += 1  # add 1 to magic
+                magic = character_stats[2]
                 skillpoints -= 1
             elif increase_skill.lower() == "dexterity":
                 print("Dex increased. What else?")
                 character_stats[3] += 1  # add 1 to dex
+                dexterity = character_stats[3]
                 skillpoints -= 1
             else:
                 print("Error. Please choose either: Strength, Magic or Dexterity") #validation
 
         print("Your stats have increased. You now have: ")
         print("HP: ",character_stats[0], "\nStrength: ",character_stats[1], "\nMagic: ",character_stats[2], "\nDexterity: ",character_stats[3])
-        return character_stats[:]
+        return character_stats[:], strength, magic, dexterity
     else:
         print("You have ", experience, "experience.")
 
@@ -104,13 +112,23 @@ def trap(dexterity):
         print("Too slow. The trap got you")
         return trap_dodged
 
-
+#check weak type function
+def check_effective(attack_type, weakness, damage):
+    if attack_type == weakness:
+        damage = damage * 1.3
+        print("Your weapon is highly effective against this foe")
+    return damage
 # ------------ define functions for each weapon --------------
 # sword function
-def sword_attack():
-    damage = 7 + strength
+def sword_attack(character_stats, weakness):
+    attack_type = "slash"
+    strength_mod = character_stats[1]
+    damage = 7 + strength_mod
+    damage = check_effective(attack_type, weakness, damage) #call effective function
+
     hit_chance = 0.7  # 50%chance to hit
     attack_type = "slash"
+    print("You slash with your sword...")
 
     if random.random() <= hit_chance:
         print("Attack landed! You dealt", damage, "damage.")
@@ -120,10 +138,13 @@ def sword_attack():
         return 0
 
 
-def axe_attack():
+def axe_attack(strength, weakness):
+    attack_type = "cleave"
     damage = 10 + strength
-    hit_chance = 0.4  # 30%chance to hit
-    attack_type = "blunt"
+    damage = check_effective(attack_type, weakness, damage)  # call effective function
+
+    hit_chance = 0.8  # 30%chance to hit
+    print("You swing your axe!")
 
     if random.random() <= hit_chance:
         print("Attack landed! You dealt", damage, "damage.")
@@ -133,10 +154,12 @@ def axe_attack():
         return 0
 
 
-def bow_attack():
-    damage = 10 + dexterity
-    hit_chance = 0.7
+def bow_attack(dexterity, weakness):
     attack_type = "ranged"
+    damage = 10 + dexterity
+    damage = check_effective(attack_type, weakness, damage)  # call effective function
+
+    hit_chance = 0.7
 
     if random.random() <= hit_chance:
         print("Attack landed! You dealt", damage, "damage.")
@@ -146,10 +169,12 @@ def bow_attack():
         return 0
 
 
-def staff_attack():
-    damage = 13 + magic
-    hit_chance = 0.5
+def staff_attack(character_stats, weakness):
     attack_type = "magic"
+    damage = 13 + magic
+    damage = check_effective(attack_type, weakness, damage)  # call effective function
+    hit_chance = 0.5
+
 
     if random.random() <= hit_chance:
         print("Attack landed! You dealt", damage, "damage.")
@@ -158,11 +183,20 @@ def staff_attack():
         print("Bad luck! You missed")
         return 0
 
+#create dictionary mapping each attack function its weapon
+
+weapon_attacks = {
+    "sword":sword_attack,
+    "axe": axe_attack,
+    "bow": bow_attack,
+    "staff": staff_attack
+}
 
 #----define goblin class and function for attack----#
 class Goblin:
     def __init__(self):
         self.health = 50
+        self.weakness = "cleave"
 
     def attack(self):
         damage = 2
@@ -179,7 +213,7 @@ class Goblin:
 class Troll:
     def __init__(self):
         self.health = 50 #troll health
-        self.weakness = "magic" #weakness to magic
+        self.weakness = "cleave" #weakness to magic
 
     def attack(self):
         hit_chance = 0.5
@@ -195,14 +229,18 @@ class Troll:
 
 
 # begin the game
-adventure = input(
-    "You are now fully equipped and ready for adventure.\n While travelling you encounter a mysterious looking cave. Do you wish to enter? \nYes \nNo \n").lower()
-while adventure == "yes":
-    print("A goblin sneaks up behind you and attacks you!")
+print("You are now fully equipped and ready for adventure.\n")
+print("You stumble upon a mysterious looking cave")
+adventure = input("Do you wish to enter?\nYes \nNo \n").lower()
+if adventure == "no":
+    print("Scared already?\n a strong wind forces you inside")
+while adventure:
+    print("Suddenly a goblin sneaks up behind you and attacks you!")
 
     # begin fight simulation
     health_points = character_stats[0]  # base HP
     goblin = Goblin()  # create instance of goblin to fight
+    weakness = goblin.weakness
 
     while health_points > 0 and goblin.health > 0:
 
@@ -216,62 +254,23 @@ while adventure == "yes":
             break
 
         # players turn
-        if char_weapon.lower() == "sword":  # check if weapon sword
-            print("You slash with your sword...")
-            player_damage = sword_attack()  # call sword attack function
-            goblin.health -= player_damage
-            print("The goblin has", goblin.health, "health remaining")
-
-            if goblin.health <= 0:
-                print("Congratulations! You slayed that goblin bish")
-                adventure = False
-                break
-
-        elif char_weapon.lower() == "axe":  # call axe attack function
-            print("You swing your axe!")
-            player_damage = axe_attack()
-            goblin.health -= player_damage
-            print("The goblin has", goblin.health, "health remaining")
-
-            if goblin.health <= 0:
-                print("Congratulations! You slayed that goblin bish")
-                adventure = False
-                break
-
-        elif char_weapon.lower() == "bow":  # call bow
-            print("You fire your bow...")
-            player_damage = bow_attack()
-            goblin.health -= player_damage
-            print("The goblin has", goblin.health, "health remaining")
-
-            if goblin.health <= 0:
-                print("Congratulations! You slayed that goblin bish")
-                adventure = False
-                break
-
         else:
-            print("You cast a spell using your staff!")
-            player_damage = staff_attack()
+            player_attack = weapon_attacks[char_weapon] #check which weapon to attack with
+            player_damage = player_attack(character_stats, weakness) #call weapon function
             goblin.health -= player_damage
-            print("The goblin has", goblin.health, "health remaining")
-
             if goblin.health <= 0:
-                print("Congratulations! You slayed the goblin.")
-                dexterity = dexterity + 2
-
-
+                print("You slayed that goblin bish")
                 adventure = False
                 break
 
-if health_points <= 0:
-    print("GAME OVER")
 
 else:
     experience = experience + 10 #gain experience
     level_up(character_stats, experience, next_level) #call levelup
-    health_points = character_stats[0]
+    health_points = character_stats[0] #reset health
 
-    print("As you walk over the Goblins dead body you notice a shiny key. Do you wish to take the key?")
+    print("As you walk over the Goblins dead body you notice a shiny key.")
+    print("Do you wish to take the key?")
 chapter_two = input()
 #while chapter_two.lower() == "yes":
 # begin chapter 2 of game
@@ -288,13 +287,34 @@ else:
     print("You manage to grab on the door, narrowly avoiding death")
 
 print("As you regain composure, you hear heavy footsteps approaching and a strange gargling sound")
-troll = Troll() #initialise a troll instance
-while health_points > 0 and troll.health > 0:
-    troll_damage = troll.attack()
-    health_points -= troll_damage
-    print("You have", health_points, "health points remaining")
-    if health_points <= 0:
-        print("Oh no.. the Troll slayed you. YOU DIED")
-        break
-    elif troll.health <= 0:
-        print("Wow you slayed that troll!")
+print("Do you wish to investigate?")
+answer = input().lower()
+
+#begin combat with troll
+if answer == "yes":
+    troll = Troll()  # initialise a troll instance
+    weakness = troll.weakness
+    print("A disgusting 7ft troll charges at you")
+
+    while health_points > 0 and troll.health > 0 and not progress:
+
+        #trolls turn
+        troll_damage = troll.attack()
+        health_points -= troll_damage
+        print("You have", health_points, "health points remaining")
+        if health_points <= 0:
+            print("Oh no.. the Troll slayed you. YOU DIED")
+            break
+
+        #players turn
+        player_attack = weapon_attacks[char_weapon] #check weapon equipped
+        player_damage = player_attack(character_stats, weakness) #attack with stats as arguments
+        troll.health -= player_damage
+        if troll.health <= 0:
+            print("The Troll crumbles beneath your weapon and dies")
+            progress = True
+
+
+else:
+    print("You got scared and escaped. Coward") #voluntary game over
+
